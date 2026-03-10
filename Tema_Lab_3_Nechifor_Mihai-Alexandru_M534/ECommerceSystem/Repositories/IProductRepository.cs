26ebd71c238da02859f892c630ce30eecf09575c
nechiforalexandru-uaic <urmomgay6942099@yahoo.com>
2026-03-10 20:22:25 +0200

    Add files via upload

---
+++ Tema_Lab_3_Nechifor_Mihai-Alexandru_M534/ECommerceSystem/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using ECommerceSystem.Models;
+
+namespace ECommerceSystem.Repositories
+{
+    public interface IProductRepository
+    {
+        Product GetById(int id);
+        List<Product> GetAll();
+        void Add(Product product);
+    }
+}
---
+++ Tema_Lab_3_Nechifor_Mihai-Alexandru_M534/ECommerceSystem/Repositories/ProductRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ECommerceSystem.Models;
+
+namespace ECommerceSystem.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private List<Product> _products;
+
+        public ProductRepository()
+        {
+            _products = new List<Product>();
+        }
+
+        public Product GetById(int id)
+        {
+            return _products.FirstOrDefault(p => p.Id == id);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products.ToList();
+        }
+
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+    }
+}
